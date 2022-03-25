daf67b1967e19d08d9e3d3dae48de8125fb71837
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-25 19:00:37 +0300

    Make changes to the urls to use path instead of re_path

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'Gallery.html')
+    return render(request, 'Gallery/gallery.html')
+    # return HttpResponse('Welcome to my Gallery')
