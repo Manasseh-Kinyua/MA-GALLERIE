80530f75e79aca341ea0cce4b03b80fa06d9eadd
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-26 17:26:47 +0300

    register bootstrap5 to installed apps

--- GalleryProject/settings.py
+++ GalleryProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Gallery.apps.GalleryConfig'
+    'Gallery.apps.GalleryConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
