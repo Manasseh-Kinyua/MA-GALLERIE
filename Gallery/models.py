e97614f3300cc1fd0c334288b73b2e9ac630787d
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-26 19:13:17 +0300

    Install pillow

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/', null = True)
+    image_name = models.CharField(max_length = 50)
+    description = models.TextField()
+
