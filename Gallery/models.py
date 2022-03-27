ffd83259089d3f6030a48d78492fd1d0db1cd268
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-27 09:18:07 +0300

    Make the class Category

--- Gallery/models.py
+++ Gallery/models.py
@@ -6,3 +6,14 @@ class Image(models.Model):
     image_name = models.CharField(max_length = 50)
     description = models.TextField()
 
+    def __str__(self):
+        return self.image_name
+
+class Category(models.Model):
+    location = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.location
+
+
+
