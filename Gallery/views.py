22bd723b89286a95430135541cf5079398ad3d3d
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-27 13:25:26 +0300

    Add images to the gallery view function to test if the database is working

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category,Location,Image
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'Gallery/gallery.html')
-    # return HttpResponse('Welcome to my Gallery')
+    images = Image.objects.all()
+    return render(request, 'Gallery/gallery.html', {"images": images})
