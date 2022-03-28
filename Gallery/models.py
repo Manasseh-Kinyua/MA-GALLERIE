4c6104304a91d881f7fb8400297c5cbbcaec704d
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-28 16:54:27 +0300

    Add delete, save and update methods for Location model

--- Gallery/models.py
+++ Gallery/models.py
@@ -20,10 +20,17 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
+    def delete_location(self):
+        self.delete()
+
     @classmethod
     def get_locations(cls):
         locations = Location.objects.all()
         return locations
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
 
     def __str__(self):
         return self.location
