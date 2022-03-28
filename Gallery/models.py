e2e87cae7e41cfbecbcad258c51cde452f543878
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-28 16:53:07 +0300

    Add the Ctegory methods for saving and deleting

--- Gallery/models.py
+++ Gallery/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length = 50)
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
     def __str__(self):
         return self.name
 
