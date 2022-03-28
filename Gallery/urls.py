d3df12b03b81e2dd87181444404372f25099608b
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-03-28 13:56:17 +0300

    Add the url fpor the photo page

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.gallery, name = 'gallery'),
-    path('search/', views.search_results, name = 'search_results')
+    path('photo/', views.photo, name = 'photo'),
+    path('search/', views.search_results, name = 'search_results'),
 ]
 
 if settings.DEBUG:
